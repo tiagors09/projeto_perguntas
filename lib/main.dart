d2c8d8f7fc24fca09cc1687afea8f2191a2093ba
Tiago Rodrigues Sousa <tiagorodriguessousa9@gmail.com>
2023-12-26 18:53:58 -0300

    creating questions and answers

--- lib/main.dart
+++ lib/main.dart
@@ -1,68 +1,42 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(const MyApp());
+main() {
+  runApp(PerguntaApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
+class PerguntaApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    final List<String> perguntas = [
+      'Qual é a sua cor favorita?',
+      'Qual é o seu animal favorito?',
+    ];
+
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Perguntas'),
+        ),
+        body: Column(
+          children: [
+            Text(perguntas.elementAt(0)),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child:
+                  ElevatedButton(onPressed: () {}, child: Text('Resposta 1')),
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child:
+                  ElevatedButton(onPressed: () {}, child: Text('Resposta 1')),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child:
+                  ElevatedButton(onPressed: () {}, child: Text('Resposta 1')),
             ),
           ],
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
       ),
     );
   }
